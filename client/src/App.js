3afaf6cdfd7020d00f8c46773d628dbbb012c880
unknown <jagins629@gmail.com>
2020-12-20 18:22:45 -0600

    top menu bar is done will just have to make the search bar work later

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import MenuBar from './components/MenuBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MenuBar/>
     </div>
   );
 }
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block'
+        }
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15), 
+        '&hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25)
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto'
+        }
+    },
+    SearchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputRoot: {color: 'inherit'},
+    inputInput: {
+        padding: theme.spacing(1,1,1,0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch'
+            }
+        }
+    }
+}));
+
+function MenuBar()
+{
+    const classes = useStyles()
+    return (
+        <AppBar className={classes.root}>
+            <ToolBar>
+                <Typography className={classes.title} variant='h6' noWrap>The Shoppies</Typography>
+                <div className={classes.search}>
+                    <div className={classes.SearchIcon}>
+                        <SearchIcon/>
+                    </div>
+                    <InputBase
+                        placeholder='Search...'
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput
+                        }}
+                        inputProps={{'aria-label': 'search'}}
+                    />
+                </div>
+            </ToolBar>
+        </AppBar>
+    )
+}
+
+export default MenuBar;
