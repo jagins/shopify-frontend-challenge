fbdbcfa0fb6587289efe6bc592af020f47728c5d
unknown <jagins629@gmail.com>
2020-12-22 18:18:35 -0600

    added an alert when user has added 5 movies to their list

--- client/src/components/NominationList.js
+++ client/src/components/NominationList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import swal from 'sweetalert';
 
 function NominationList(props)
 {
     const {nominatedMovies} = props;
+    if(nominatedMovies.length >= 5)
+    {
+        swal('Success', `You've added 5 movies to your nomination list`, 'success');
+    }
     return (
         <div className='nomination-list'>
            {nominatedMovies.length > 0 && <h3>Nominated Movies</h3>}
