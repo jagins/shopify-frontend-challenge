5e31269a3654a23e704408e8bff3c1ccdadcb8eb
unknown <jagins629@gmail.com>
2020-12-26 12:11:27 -0600

    changed from heart to regular button need to fix the remove from list to make the button enabled again

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -4,11 +4,9 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import { red } from '@material-ui/core/colors';
 import {connect} from 'react-redux';
 import {addMovieToList} from '../actions';
+import { Button } from '@material-ui/core';
 
 function MovieCard(props) {
     const {addMovieToList, nominatedMovies} = props;
@@ -27,9 +25,7 @@ function MovieCard(props) {
             <CardHeader title={`${props.movie.Title} (${props.movie.Year})`}/>
             <CardMedia className={classes.media} image={props.movie.Poster} />
             <CardActions disableSpacing>
-                <IconButton aria-label='add to favorites' onClick={handleClick}>
-                    {!clicked ? <FavoriteIcon /> : <FavoriteIcon style={{color: red[500]}}/>}
-                </IconButton>
+               {clicked ? <Button disabled>Nominate</Button> : <Button onClick={handleClick}>Nominate</Button>}
             </CardActions>
         </Card>
     )
